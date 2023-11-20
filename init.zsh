4a69bdbe9baf5a8765f36998f53eae80067d6c79
Danielle McLean <danielle@ethicaljobs.com.au>
2023-11-20 12:35:13 +1100

    Attempt to set HELPDIR automatically

--- init.zsh
+++ init.zsh
@@ -3,6 +3,11 @@ autoload -Uz run-help
 bindkey '^[h' run-help
 bindkey '^[H' run-help
 
+if (( ! ${+HELPDIR} )); then
+  if [[ -d /usr/share/zsh/help ]] typeset -g HELPDIR=/usr/share/zsh/help
+  if [[ -d /usr/local/share/zsh/help ]] typeset -g HELPDIR=/usr/local/share/zsh/help
+fi
+
 local cmd
 for cmd in git ip openssl p4 sudo svk svn; do
   if (( ${+commands[${cmd}]} )) autoload -Uz run-help-${cmd}
